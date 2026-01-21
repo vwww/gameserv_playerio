6b42d9dea5e79e9a7956446ed5797bfd5d8c3723
Victor <theonlypwner@users.noreply.github.com>
2026-01-20 21:17:44 -0700

    feat: throttle rename

--- common/common/CommonGame.Name.cs
+++ common/common/CommonGame.Name.cs
@@ -1,5 +1,10 @@
+using Timer = PlayerIO.GameLibrary.Timer;
+
 partial class Player {
 	public string name;
+	public string nameNew;
+	public DateTime nameChangeTime;
+	public Timer nameTimer;
 }
 
 partial class Room {
@@ -7,13 +12,22 @@ partial class Room {
 
 	void ProcessMsgRename(Player player, ByteReader msg) {
 		var newName = Util.FilterName(msg.GetString(MAX_NAME_LEN), MAX_NAME_LEN);
-		if (player.name != newName) {
-			player.name = newName;
+		if (player.nameNew == newName) return;
 
-			Broadcast(new ByteWriter()
-				.PutType(MsgS2C.RENAME)
-				.PutInt(player.cn)
-				.PutString(player.name));
-		}
+		player.nameNew = newName;
+
+		player.nameTimer ??= ScheduleCallback(() => {
+			lock (players) {
+				if (player.cn >= 0 && player.name != player.nameNew) {
+					Broadcast(new ByteWriter()
+						.PutType(MsgS2C.RENAME)
+						.PutInt(player.cn)
+						.PutString(player.name = player.nameNew));
+				}
+
+				player.nameTimer = null;
+				player.nameChangeTime = DateTime.UtcNow;
+			}
+		}, (int)Math.Max(2000 - (DateTime.UtcNow - player.nameChangeTime).TotalMilliseconds, 25));
 	}
 }
